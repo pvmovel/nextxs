7ceb27fce9cfb730051d46902c7cdbb398af91a0
Pablo Valença <pablo.valenca@bfcdigital.com.br>
2022-07-22 00:36:26 -0300

    .

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,5 +1,47 @@
-const UsersPage = () => {
-  return <h1>UsersPage</h1>
-}
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { GetServerSideProps, NextPage } from "next";
+
+type User = {
+  name: string;
+};
+
+type UserPageProps = {
+  users: User[];
+};
+
+const url = "http://localhost:3001/db.json"
+
+const UsersPage: NextPage<UserPageProps> = (props) => {
+  // const { users } = props;
+  const [users, setUsers] = useState([]);
+
+   useEffect(() => {
+     axios
+       .get(url)
+       .then((response) => setUsers(response.data));
+   }, []);
+
+  return (
+    <div>
+      <h1>UsersPage</h1>
+      <ul>
+        {users.map((user: any, key) => (
+          <li key={key}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default UsersPage;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { data } = await axios.get(url);
+  
+  return {
+    props: {
+        users: data
+    }
+  };
+};
